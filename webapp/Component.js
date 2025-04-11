3a1a44261328e9e55a962591d23e94503ec62280
AS RAO <asrao@ssrbusinesssolutions.com>
2025-04-11 07:45:59 +0000

    stable version

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("vod.purchaseorderapp.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
